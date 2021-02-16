a81bdae3324525c5554d684b71639c874a837921
Travis <46234513+XuulMedia@users.noreply.github.com>
2021-02-15 19:48:48 -0700

    changed name scheme

--- src/main/java/com/xuul/soulsmith/registry/ModBlocks.java
+++ src/main/java/com/xuul/soulsmith/registry/ModBlocks.java
@@ -5,65 +5,68 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
-
-    public static final Block RYKLE_BLOCK = new Block(FabricBlockSettings
-            .of(Material.GLASS)
-            .breakByTool(FabricToolTags.PICKAXES, 1)
-            .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.LANTERN)
-    );
-    public static final Block TIN_BLOCK = new Block(FabricBlockSettings
-            .of(Material.METAL)
-            .breakByTool(FabricToolTags.PICKAXES, 1)
-            .requiresTool()
-            .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.METAL)
-    );
-    public static final Block BRONZE_BLOCK = new Block(FabricBlockSettings
-            .of(Material.METAL)
-            .breakByTool(FabricToolTags.PICKAXES, 1)
-            .requiresTool()
-            .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.METAL)
-    );
-    public static final Block STEEL_BLOCK = new Block(FabricBlockSettings
-            .of(Material.METAL)
-            .breakByTool(FabricToolTags.PICKAXES, 2)
-            .requiresTool()
-            .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.METAL)
-    );
-    public static final Block BLACK_BRONZE_BLOCK = new Block(FabricBlockSettings
-            .of(Material.METAL)
-            .breakByTool(FabricToolTags.PICKAXES, 1)
-            .requiresTool()
-            .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.METAL)
-    );
-    public static final Block SILVER_BLOCK = new Block(FabricBlockSettings
-            .of(Material.METAL)
-            .breakByTool(FabricToolTags.PICKAXES, 1)
-            .requiresTool()
-            .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.METAL)
+    public static final OreBlock ORE_TIN = new OreBlock(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 2)
+            .requiresTool().strength(3.0f,3.0f).sounds(BlockSoundGroup.STONE)
     );
 
+    public static final OreBlock ORE_NETHER_SILVER = new OreBlock(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 2)
+            .requiresTool().strength(3.0f,3.0f).sounds(BlockSoundGroup.NETHER_GOLD_ORE)
+    );
+
+    public static final OreBlock ORE_RYKLE = new OreBlock(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 2)
+            .requiresTool().strength(3.0f,3.0f).sounds(BlockSoundGroup.STONE)
+    );
+
+    public static final OreBlock ORE_SHROUD_CRYSTAL = new OreBlock(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 3)
+            .requiresTool().strength(3.0f,3.0f).sounds(BlockSoundGroup.NETHER_GOLD_ORE)
+    );
+
+    public static final OreBlock ORE_TYRELLUS = new OreBlock(FabricBlockSettings.of(Material.STONE).breakByTool(FabricToolTags.PICKAXES, 4)
+            .requiresTool().strength(3.0f,3.0f).sounds(BlockSoundGroup.STONE)
+    );
+
+
+    public static final Block BLOCK_RYKLE = new Block(FabricBlockSettings.of(Material.GLASS).breakByTool(FabricToolTags.PICKAXES, 1)
+            .strength(3.0F, 3.0F).sounds(BlockSoundGroup.LANTERN)
+    );
+    public static final Block BLOCK_TIN = new Block(FabricBlockSettings.of(Material.METAL).breakByTool(FabricToolTags.PICKAXES, 1)
+            .requiresTool().strength(3.0F, 3.0F).sounds(BlockSoundGroup.METAL)
+    );
+    public static final Block BLOCK_BRONZE = new Block(FabricBlockSettings.of(Material.METAL).breakByTool(FabricToolTags.PICKAXES, 1)
+            .requiresTool().strength(3.0F, 3.0F).sounds(BlockSoundGroup.METAL)
+    );
+    public static final Block BLOCK_STEEL = new Block(FabricBlockSettings.of(Material.METAL).breakByTool(FabricToolTags.PICKAXES, 2)
+            .requiresTool().strength(3.0F, 3.0F).sounds(BlockSoundGroup.METAL)
+    );
+    public static final Block BLOCK_BLACK_BRONZE = new Block(FabricBlockSettings.of(Material.METAL).breakByTool(FabricToolTags.PICKAXES, 1)
+            .requiresTool().strength(3.0F, 3.0F).sounds(BlockSoundGroup.METAL)
+    );
+    public static final Block BLOCK_SILVER = new Block(FabricBlockSettings.of(Material.METAL).breakByTool(FabricToolTags.PICKAXES, 1)
+            .requiresTool().strength(3.0F, 3.0F).sounds(BlockSoundGroup.METAL)
+    );
 
 
     public static void registerItems() {
-        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "rykle_block"), RYKLE_BLOCK);
 
-        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "tin_block"), TIN_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "bronze_block"), BRONZE_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "steel_block"), STEEL_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "black_bronze_block"), BLACK_BRONZE_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "silver_block"), SILVER_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "ore_tin"), ORE_TIN);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "ore__nether_silver"), ORE_NETHER_SILVER);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "ore_rykle"), ORE_RYKLE);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "ore_shroud_crystal"), ORE_SHROUD_CRYSTAL);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "ore_tyrellus"), ORE_TYRELLUS);
+
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "block_rykle"), BLOCK_RYKLE);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "block_tin"), BLOCK_TIN);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "block_bronze"), BLOCK_BRONZE);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "block_steel"), BLOCK_STEEL);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "block_black_bronze"), BLOCK_BLACK_BRONZE);
+        Registry.register(Registry.BLOCK, new Identifier(Soulsmith.MOD_ID, "block_silver"), BLOCK_SILVER);
 
     }
 }
--- src/main/java/com/xuul/soulsmith/registry/ModItems.java
+++ src/main/java/com/xuul/soulsmith/registry/ModItems.java
@@ -13,52 +13,81 @@ public class ModItems {
     public static final Item SHROUD = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
     public static final Item TYRELLUS = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
 
-    public static final Item TIN_INGOT = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item BRONZE_INGOT = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item STEEL_INGOT = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item SILVER_INGOT = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item BLACK_BRONZE_INGOT = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item INGOT_TIN = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item INGOT_BRONZE = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item INGOT_STEEL = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item INGOT_SILVER = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item INGOT_BLACK_BRONZE = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
 
-    public static final Item COPPER_NUGGET = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item TIN_NUGGET = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item BRONZE_NUGGET = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item STEEL_NUGGET = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final Item SILVER_NUGGET = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item NUGGET_COPPER = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item NUGGET_TIN = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item NUGGET_BRONZE = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item NUGGET_STEEL = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final Item NUGGET_SILVER = new Item(new Item.Settings().group(Soulsmith.ITEM_GROUP));
 
 
     //Block Items
-    public static final BlockItem RYKLE_BLOCK = new BlockItem(ModBlocks.RYKLE_BLOCK, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final BlockItem TIN_BLOCK = new BlockItem(ModBlocks.TIN_BLOCK, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final BlockItem BRONZE_BLOCK = new BlockItem(ModBlocks.BRONZE_BLOCK, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final BlockItem STEEL_BLOCK = new BlockItem(ModBlocks.STEEL_BLOCK, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final BlockItem SILVER_BLOCK = new BlockItem(ModBlocks.SILVER_BLOCK, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-    public static final BlockItem BLACK_BRONZE_BLOCK = new BlockItem(ModBlocks.BLACK_BRONZE_BLOCK, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem BLOCK_RYKLE = new BlockItem(ModBlocks.BLOCK_RYKLE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem BLOCK_TIN = new BlockItem(ModBlocks.BLOCK_TIN, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem BLOCK_BRONZE = new BlockItem(ModBlocks.BLOCK_BRONZE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem BLOCK_STEEL = new BlockItem(ModBlocks.BLOCK_STEEL, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem BLOCK_SILVER = new BlockItem(ModBlocks.BLOCK_SILVER, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem BLOCK_BLACK_BRONZE = new BlockItem(ModBlocks.BLOCK_BLACK_BRONZE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
 
-//    public static final BlockItem RYKLE_ORE = new BlockItem(ModBlocks.RYKLE_ORE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-//    public static final BlockItem SHROUD_CRYSTAL_ORE = new BlockItem(ModBlocks.SHROUD_CRYSTAL_ORE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-//    public static final BlockItem TYRELLUS_ORE = new BlockItem(ModBlocks.TYRELLUS_ORE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-//    public static final BlockItem TIN_ORE = new BlockItem(ModBlocks.TIN_ORE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
-//    public static final BlockItem SILVER_ORE = new BlockItem(ModBlocks.SILVER_ORE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem ORE_RYKLE = new BlockItem(ModBlocks.ORE_RYKLE, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem ORE_SHROUD_CRYSTAL = new BlockItem(ModBlocks.ORE_SHROUD_CRYSTAL, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem ORE_TYRELLUS = new BlockItem(ModBlocks.ORE_TYRELLUS, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem ORE_TIN = new BlockItem(ModBlocks.ORE_TIN, new Item.Settings().group(Soulsmith.ITEM_GROUP));
+    public static final BlockItem ORE_SILVER = new BlockItem(ModBlocks.ORE_NETHER_SILVER, new Item.Settings().group(Soulsmith.ITEM_GROUP));
 
 
 
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID, "rykle"), RYKLE);
-
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"rykle"), RYKLE);
         Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"shroud_crystal"), SHROUD);
         Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"tyrellus"), TYRELLUS);
 
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"tin_ingot"), TIN_INGOT);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"bronze_ingot"), BRONZE_INGOT);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"steel_ingot"), STEEL_INGOT);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"silver_ingot"), SILVER_INGOT);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"black_bronze_ingot"), BLACK_BRONZE_INGOT);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ingot_tin"), INGOT_TIN);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ingot_bronze"), INGOT_BRONZE);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ingot_steel"), INGOT_STEEL);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ingot_silver"), INGOT_SILVER);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ingot_black_bronze"), INGOT_BLACK_BRONZE);
 
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"copper_nugget"), COPPER_NUGGET);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"tin_nugget"), TIN_NUGGET);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"bronze_nugget"), BRONZE_NUGGET);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"steel_nugget"), STEEL_NUGGET);
-        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"silver_nugget"), SILVER_NUGGET);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"nugget_copper"), NUGGET_COPPER);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"nugget_tin"), NUGGET_TIN);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"nugget_bronze"), NUGGET_BRONZE);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"nugget_steel"), NUGGET_STEEL);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"nugget_silver"), NUGGET_SILVER);
+
+
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"block_rykle"), BLOCK_RYKLE);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"block_tin"), BLOCK_TIN);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"block_bronze"), BLOCK_BRONZE);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"block_steel"), BLOCK_STEEL);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"block_silver"), BLOCK_SILVER);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"block_black_bronze"), BLOCK_BLACK_BRONZE);
+
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ore_rykle"), ORE_RYKLE);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ore_shroud_crystal"), ORE_SHROUD_CRYSTAL);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ore_tyrellus"), ORE_TYRELLUS);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ore_tin"), ORE_TIN);
+        Registry.register(Registry.ITEM, new Identifier(Soulsmith.MOD_ID,"ore_silver"), ORE_SILVER);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
